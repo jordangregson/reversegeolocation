34b0aa711b63bc1ce727731b5dd752bad133460c
Jordan Gregson <49405277+jordangregson@users.noreply.github.com>
2020-07-28 12:05:29 -0400

    updates

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-  function geocodeLatLng(geocoder, map, infowindow) {
-    const input = document.getElementById("latlng").value;
-    const latlngStr = input.split(",", 2);
-    const latlng = {
-      lat: parseFloat(latlngStr[0]),
-      lng: parseFloat(latlngStr[1])
-    };
+function geocodeLatLng(geocoder, map, infowindow) {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            position => {
+                let lat = position.coords.latitude;
+                let lon = position.coords.longitude;
+            })
+    }
     geocoder.geocode({ location: latlng }, (results, status) => {
-      if (status === "OK") {
-        if (results[0]) {
-          map.setZoom(11);
-          const marker = new google.maps.Marker({
-            position: latlng,
-            map: map
-          });
-          infowindow.setContent(results[0].formatted_address);
-          infowindow.open(map, marker);
+        if (status === "OK") {
+            if (results[0]) {
+                map.setZoom(11);
+                const marker = new google.maps.Marker({
+                    position: latlng,
+                    map: map
+                });
+                infowindow.setContent(results[0].formatted_address);
+                infowindow.open(map, marker);
+                console.log(results[0].formatted_address);
+            } else {
+                window.alert("No results found");
+            }
         } else {
-          window.alert("No results found");
+            window.alert("Geocoder failed due to: " + status);
         }
-      } else {
-        window.alert("Geocoder failed due to: " + status);
-      }
     });
-  }
+}
+
+geocodeLatLng();
